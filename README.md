bc77410f9fb43c538dfcc3bc94954a79b084bc96
Chaltu Abdulkadir <chaltumabdulkadir@gmail.com>
2021-03-18 22:06:58 -0500

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Fitness_tracker
+
+## Description 
+
+For this assignment, you'll create a workout tracker. You have already been provided with the front end code in the Develop folder. This assignment will require you to create Mongo database with a Mongoose schema and handle routes with Express.
+
+## Installation
+
+First run npm i in order to install the dependencies. The application itself can be invoked with node server.js in the intergreated termeinal.
+
+## Usage
+
+Here is a link to the deployed link on heroku:
+
+https://fitness2021.herokuapp.com/
--- server.js
+++ server.js
@@ -1,34 +1,29 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-app.use(require("./routes/htmlroutes"));
-app.use(require("./routes/apiroutes"));
-
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
   }
-)
-.then(()=>{console.log("connected to db")})
-.catch(err => console.log(err));
+);
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
-  
+  console.log(`App running on port ${PORT}!`);
+});
