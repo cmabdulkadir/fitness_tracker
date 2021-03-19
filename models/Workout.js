c1acf1b063e5a5e82540e7332e7acb2a83a1794c
Chaltu Abdulkadir <chaltumabdulkadir@gmail.com>
2021-03-18 21:14:04 -0500

    updated files

--- models/Workout.js
+++ models/Workout.js
@@ -1,24 +1,38 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
-    },
-    exercises: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: "Exercise"
+var workoutSchema = new Schema({
+    day: { type: Date, required: true, default: new Date(new Date().setDate(new Date().getDate())) },
+
+    exercises: [{
+        type: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        distance : {
+            type: Number,
         }
-      ],
-      totalDuration: {
-        type: Number,
-        default: 0
-      }
-    
+
+    }]
 });
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+var Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
